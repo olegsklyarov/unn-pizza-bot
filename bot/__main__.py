63abc90682178609746a0c8bcce62f1eaccf397a
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2025-12-06 18:48:23 +0300

    middleware updates logger

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,9 +1,12 @@
 import asyncio
+import json
 import logging
 import os
 
 from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message
+from aiogram.types import Message, Update
+from aiogram.utils.serialization import deserialize_telegram_object_to_python
+
 import dotenv
 
 
@@ -22,6 +25,13 @@ async def message_text_echo_handler(message: Message) -> None:
 async def message_photo_echo_handler(message: Message) -> None:
     await message.answer_photo(message.photo[-1].file_id)
     # await message.send_copy(chat_id=message.chat.id)
+
+
+@dp.update.outer_middleware()
+async def database_transaction_middleware(handler: callable, event: Update, data: dict):
+    payload = deserialize_telegram_object_to_python(event)
+    print(json.dumps(payload, ensure_ascii=False, indent=2))
+    return await handler(event, data)
 
 
 async def main() -> None:
