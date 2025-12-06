bce7ce9a0405bd9e8c435ddff2ebb336b4a718db
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2025-12-06 19:22:10 +0300

    FSM first steps of pizza order

--- bot/__main__.py
+++ bot/__main__.py
@@ -4,7 +4,16 @@ import logging
 import os
 
 from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message, Update
+from aiogram.types import (
+    CallbackQuery,
+    Message,
+    Update,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+)
+from aiogram.filters import CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 from aiogram.utils.serialization import deserialize_telegram_object_to_python
 
 import dotenv
@@ -14,6 +23,63 @@ dotenv.load_dotenv()
 
 
 dp = Dispatcher()
+
+
+class Order(StatesGroup):
+    wait_for_pizza_name = State()
+    wait_for_pizza_size = State()
+    wait_for_drinks = State()
+    wait_for_order_approve = State()
+    wait_for_payment = State()
+    order_finished = State()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message, state: FSMContext) -> None:
+    PIZZA_NAMES = ["margherita", "pepperoni", "capricciosa", "diavola", "prosciutto"]
+    await message.answer(
+        "Выбери пиццу",
+        reply_markup=InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(
+                        text=f"{pizza_name.capitalize()}",
+                        callback_data=f"pizza_{pizza_name}",
+                    ),
+                ]
+                for pizza_name in PIZZA_NAMES
+            ]
+        ),
+    )
+    await state.set_state(Order.wait_for_pizza_name)
+
+
+@dp.callback_query(Order.wait_for_pizza_name)
+async def pizza_name_handler(callback: CallbackQuery, state: FSMContext, bot: Bot):
+    PIZZA_SIZES = ["small", "medium", "large", "xl"]
+    await asyncio.gather(
+        bot.answer_callback_query(callback.id),
+        bot.delete_message(
+            chat_id=callback.message.chat.id,
+            message_id=callback.message.message_id,
+        ),
+        bot.send_message(
+            chat_id=callback.message.chat.id,
+            text="Please select pizza size",
+            reply_markup=InlineKeyboardMarkup(
+                inline_keyboard=[
+                    [
+                        InlineKeyboardButton(
+                            text=f"{pizza_size.capitalize()}",
+                            callback_data=f"pizza_{pizza_size}",
+                        ),
+                    ]
+                    for pizza_size in PIZZA_SIZES
+                ]
+            ),
+        ),
+        state.set_state(Order.wait_for_drinks),
+    )
 
 
 @dp.message(F.text)
